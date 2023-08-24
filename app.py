03f8338bb5af9ab0ff81ba9d7142d5d47b089fd4
salah zaame <zaamesalah@gmail.com>
2023-08-24 20:12:37 +0000

    reducing inputs in the inreface

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask , render_template, jsonify, request
 import pickle
 import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
 #creating an app object using the Flask class
 app = Flask(__name__)
 
 #load the pickel model 
 model = pickle.load(open("breast_cancer_model.pkl", "rb"))
 
-column_names = ['diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean', 'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se', 'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst']
+data = pd.read_csv('data.csv')
+mean_data = data.drop(['radius_mean', 'texture_mean', 'perimeter_mean' , 'area_mean', 'diagnosis', 'id'], axis=1).mean()
+
 #Define the route to be home 
 #use the route() decorator to tell Flask what URL should trigger our function .
 @app.route('/')
@@ -18,7 +22,15 @@ def Home():
 @app.route("/predict", methods = ['POST'])
 def predict():
     float_features = [float(x) for x in request.form.values()] # fetching the values from the form and convert it to floats 
-    features = [np.array(float_features)] #converting to an array that has the same shape of our prediction data 
+    # features = [np.array(float_features)] #converting to an array that has the same shape of our prediction data 
+    # features = [np.concatenate([float_features[:4], mean_data.tolist()])]
+    features = float_features[:4]+mean_data.tolist()
+
+    features = [np.array(features)]
+    
+    imputer = SimpleImputer(strategy='mean')
+    features = imputer.fit_transform(features)
+
     prediction = model.predict(features) #make predictions .
 
     return render_template("index.html", prediction_text = "Breast cancer prediction {}".format(prediction))
